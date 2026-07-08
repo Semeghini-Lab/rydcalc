e263dd75a4e716f15e92e9b5ab3dbc6d7abb446d
cogrpar <owen.r.welsh@gmail.com>
2026-07-08 16:37:22 -0400

    fixed setup

--- rydcalc/setupc.py
+++ rydcalc/setupc.py
@@ -3,4 +3,4 @@ import numpy as np
 
 setup(ext_modules=[Extension("arc_c_extensions", ["arc_c_extensions.c"],
                              extra_compile_args = ['-Wall', '-O3'],
-      include_dirs=np.get_include())])
+      include_dirs=[np.get_include()])])
